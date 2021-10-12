aa6cb12cde640cb8a16e60a1e0cbd0f9e1b7bf36
Beno <moclad@hotmail.com>
2021-10-13 00:09:32 +0200

    new modal dialog component

---
+++ src/components/ModalDialog/ModalDialog.tsx
@@ -0,0 +1,96 @@
+import React, { FC, MutableRefObject } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+  Box,
+  Button,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay
+} from '@chakra-ui/react';
+import { Formiz, useForm } from '@formiz/core';
+
+export interface ModalDialogProps {
+  isOpen: boolean;
+  loading: boolean;
+  title: string;
+  closeOnOverlayClick?: boolean;
+  formId: string;
+  onCancel: () => void;
+  onConfirm: (values: any) => any | Promise<any> | void;
+}
+
+const ModalDialog: FC<ModalDialogProps> = (props) => {
+  const {
+    children,
+    isOpen,
+    onCancel,
+    onConfirm,
+    title,
+    loading,
+    closeOnOverlayClick,
+    formId,
+  } = props;
+
+  const { t } = useTranslation();
+  const generalInformationForm = useForm();
+
+  const submitFormData = async (values) => {
+    await onConfirm(values);
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onCancel();
+      }}
+      motionPreset="scale"
+      closeOnOverlayClick={closeOnOverlayClick ?? false}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        {/* <ModalCloseButton disabled={loading} /> */}
+        <Formiz
+          id={formId}
+          onValidSubmit={submitFormData}
+          connect={generalInformationForm}
+        >
+          <form noValidate onSubmit={generalInformationForm.submit}>
+            <ModalBody>{children}</ModalBody>
+            <ModalFooter>
+              <HStack spacing={4}>
+                <Button
+                  isDisabled={loading}
+                  variant="ghost"
+                  onClick={() => {
+                    onCancel();
+                  }}
+                >
+                  {t('common:actions.cancel')}
+                </Button>
+                <Button
+                  type="submit"
+                  variant="@primary"
+                  isLoading={loading}
+                  loadingText={t('common:actions.submitting')}
+                  onClick={onConfirm}
+                >
+                  {t('common:actions.confirm')}
+                </Button>
+              </HStack>
+            </ModalFooter>
+          </form>
+        </Formiz>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalDialog;
---
+++ src/components/ModalDialog/docs.stories.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { useDarkMode } from '@/hooks/useDarkMode';
+import { Button, Stack } from '@chakra-ui/react';
+import { Formiz, useForm } from '@formiz/core';
+import { isEmail } from '@formiz/validations';
+
+import { FieldInput } from '../';
+import ModalDialog from './ModalDialog';
+
+export default {
+  title: 'Components/ModalDialog',
+  component: Button,
+  argTypes: {
+    title: 'Modal Title',
+    formId: 'modal-dialog-form',
+    loading: false,
+    closeOnOverlayClick: false,
+    isOpen: true,
+    onClose: { action: 'closed' },
+    onConfirm: { action: 'confirm' },
+  },
+};
+
+const Template = ({
+  isOpen,
+  title,
+  loading,
+  closeOnOverlayClick,
+  onClose,
+  onConfirm,
+  formId,
+}) => {
+  const { colorModeValue } = useDarkMode();
+
+  return (
+    <ModalDialog
+      title={title}
+      isOpen={isOpen}
+      onCancel={() => onClose()}
+      onConfirm={(values) => onConfirm(values)}
+      loading={loading}
+      closeOnOverlayClick={closeOnOverlayClick}
+      formId={formId}
+    >
+      <Stack
+        direction="column"
+        bg={colorModeValue('white', 'blackAlpha.400')}
+        p="6"
+        borderRadius="lg"
+        spacing="6"
+        shadow="md"
+      >
+        <Stack direction={{ base: 'column', sm: 'row' }} spacing="6">
+          <FieldInput name="label1" label="Label 1" required="Field required" />
+          <FieldInput name="label2" label="Label 2" />
+        </Stack>
+        <FieldInput
+          name="email"
+          label="Email"
+          required="Email is required"
+          validations={[
+            {
+              rule: isEmail(),
+              message: 'Invalid email',
+            },
+          ]}
+        />
+      </Stack>
+    </ModalDialog>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Modal title',
+  formId: 'modal-dialog-form',
+  loading: false,
+  closeOnOverlayClick: false,
+  isOpen: true,
+};
