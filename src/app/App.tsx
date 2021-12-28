71831bc612fa12f3fa4c70f9e93e198902166891
Beno <moclad@hotmail.com>
2021-12-28 21:57:12 +0100

    Last changes before moving to next-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { PageAuthError } from '@/app/auth/PageAuthError';
 import { PageLogin } from '@/app/auth/PageLogin';
@@ -8,7 +8,7 @@ import { PageLogout } from '@/app/auth/PageLogout';
 import { PageVerifyRequest } from '@/app/auth/PageVerifyRequest';
 import ClassificationRoutes from '@/app/classification/ClassificationRoutes';
 import { Layout, Loader } from '@/app/layout';
-import { Route, RouteAdmin, RoutePublic, RoutePublicOnly } from '@/app/router';
+import { Route as CustomRoute, RouteAdmin, RoutePublicOnly } from '@/app/router';
 import { Error404, ErrorBoundary } from '@/errors';
 
 const AdminRoutes = React.lazy(() => import('@/app/admin/AdminRoutes'));
@@ -20,49 +20,39 @@ const DashboardRoutes = React.lazy(
   () => import('@/app/dashboard/DashboardRoutes')
 );
 
+const getRoutes = () => {
+  return (
+    <Switch>
+      <RoutePublicOnly
+        exact
+        path="/verify-request"
+        render={() => <PageVerifyRequest />}
+      />
+      <RoutePublicOnly exact path="/error" render={() => <PageAuthError />} />
+      <RoutePublicOnly exact path="/login" render={() => <PageLogin />} />
+
+      <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
+      <Route exact path="/logout" render={() => <PageLogout />} />
+      <Route path="/account" render={() => <AccountRoutes />} />
+      <Route path="/profile" render={() => <AccountRoutes />} />
+      <Route path="/dashboard" render={() => <DashboardRoutes />} />
+      <Route path="/classification" render={() => <ClassificationRoutes />} />
+      <Route path="/accounts" render={() => <AccountsRoutes />} />
+
+      <Route path="*" render={() => <Error404 />} />
+
+      <RouteAdmin path="/admin" render={() => <AdminRoutes />} />
+      <CustomRoute path="/dashboard" render={() => <DashboardRoutes />} />
+    </Switch>
+  );
+};
+
 export const App = () => {
   return (
     <ErrorBoundary>
       <BrowserRouter basename="/app/">
         <Layout>
-          <Suspense fallback={<Loader />}>
-            <Switch>
-              <RoutePublic
-                exact
-                path="/"
-                render={() => <Redirect to="/dashboard" />}
-              />
-
-              <RoutePublicOnly
-                exact
-                path="/login"
-                render={() => <PageLogin />}
-              />
-              <RoutePublic exact path="/logout" render={() => <PageLogout />} />
-              <RoutePublicOnly
-                exact
-                path="/verify-request"
-                render={() => <PageVerifyRequest />}
-              />
-              <RoutePublicOnly
-                exact
-                path="/error"
-                render={() => <PageAuthError />}
-              />
-
-              <RoutePublic path="/profile" render={() => <AccountRoutes />} />
-
-              <Route path="/dashboard" render={() => <DashboardRoutes />} />
-              <Route
-                path="/classification"
-                render={() => <ClassificationRoutes />}
-              />
-              <Route path="/accounts" render={() => <AccountsRoutes />} />
-              <RouteAdmin path="/admin" render={() => <AdminRoutes />} />
-
-              <RoutePublic path="*" render={() => <Error404 />} />
-            </Switch>
-          </Suspense>
+          <Suspense fallback={<Loader />}>{getRoutes()}</Suspense>
         </Layout>
       </BrowserRouter>
       <ReactQueryDevtools />
