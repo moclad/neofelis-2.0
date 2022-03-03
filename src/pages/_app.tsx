559627e9e7edb3d83b918b06267314a3eb164cc7
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2022-03-03 10:22:37 +0100

    Update meta viewport (#184)
    Remove the `user-scalable=no` for better accessibility
    Add `width=device-width`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,7 +60,7 @@ const AppHead = () => {
       <title>Neofelis</title>
       <meta
         name="viewport"
-        content="initial-scale=1, viewport-fit=cover, user-scalable=no"
+        content="width=device-width,initial-scale=1,viewport-fit=cover"
       />
       <meta
         name="apple-mobile-web-app-status-bar-style"
